6236bbf861faf42ffbd9247eac6291ecbe839469
YuAhyeon <dkgus7123@naver.com>
2025-03-13 22:21:06 +0900

    express 서버 및 socket.io 기본 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import http from "http";
+
+import { Server } from "socket.io";
+
+// env 가져오기
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+app.use(cors());
+app.use(express.json());
+
+server.listen(PORT, () => console.log("🚀 서버 실행 중"));
