cb682236f54f4687aa9229ccc62401b9e060372e
YuAhyeon <dkgus7123@naver.com>
2025-03-13 23:08:11 +0900

    feat: mongoDB 연결 추가

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL as string);
+    console.log("✅ MongoDB 연결 성공");
+  } catch (error) {
+    console.error("❌ mongoDB 연결 확인");
+    process.exit(1);
+  }
+};
+
+export default connectDB;
