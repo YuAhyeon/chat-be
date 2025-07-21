e2e6ba8c684383ed1b25fbf496b1e45b5a2ffe59
YuAhyeon <dkgus7123@naver.com>
2025-07-22 02:39:32 +0900

    delete: 필요 없는 파일 제거

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,7 +8,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URL as string);
     console.log("✅ MongoDB 연결 성공");
   } catch (error) {
-    console.error("❌ mongoDB 연결 확인");
+    console.error("❌ MongoDB 연결 확인");
     process.exit(1);
   }
 };
--- src/routes/chatRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getMessages, postMessage } from "../controllers/chatController";
-
-const router = express.Router();
-
-router.get("/messages", getMessages);
-router.post("/messages", postMessage);
-
-export default router;
